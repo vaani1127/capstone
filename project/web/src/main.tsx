8407274ffbc4fab75f244be70c6f22cbc27104b7
vaani1127 <vprashar_be23@thapar.edu>
2026-08-06 17:18:39 +0530

    Fix PWA install detection: remove conflicting manual manifest and sw registration, add Apple meta tags

--- project/web/src/main.tsx
+++ project/web/src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').catch(error => {
-    console.log('ServiceWorker registration failed: ', error);
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
